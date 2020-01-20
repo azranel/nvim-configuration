74af9b5a37928caf5abaa4edc510e3c1f7a5f219
Bartosz Lecki <bartosz.lecki@intercom.io>
2020-01-20 13:45:28 +0000

    Add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install plug-vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install pynvim plugin for python so deoplete will work
+python2 -m pip install --user --upgrade pynvim
+python3 -m pip install --user --upgrade pynvim
